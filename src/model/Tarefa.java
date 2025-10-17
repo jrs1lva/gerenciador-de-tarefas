e09f4a7fdeb49fe149c1c2826121bc5b8d028d2a
jrs1lva <adailtonjunior0604@gmail.com>
2025-10-17 11:51:45 -0300

    feat: implements class tarefa

--- src/model/Tarefa.java
+++ src/model/Tarefa.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Tarefa {
+	
+	private int id;
+	private String titulo;
+	private String descricao;
+	private boolean concluido;
+	
+	public Tarefa(int id, String titulo, String descricao, boolean concluido) {
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.concluido = concluido;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public boolean isConcluido() {
+		return concluido;
+	}
+
+	public void setConcluido(boolean concluido) {
+		this.concluido = concluido;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: + ", titulo=" + titulo + ", descricao=" + descricao + ", concluido=" + concluido
+				+ "]";
+	}
+	
+	
+	
 }
